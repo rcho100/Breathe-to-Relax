b34e852a9f26e67c834af61be94fa8f4f3362e05
Richard Cho <richardcho100@gmail.com>
2020-10-16 00:33:12 -0400

    Add animation to grow and shrink circle dynamically

--- script.js
+++ script.js
@@ -9,11 +9,13 @@ breatheAnimation()
 
 function breatheAnimation() {
     text.innerText = "Breathe In"
+    container.className = "container grow"
 
     setTimeout(() => {
         text.innerText = "Hold"
         setTimeout(() => {
             text.innerText = "Breathe out"
+            container.className = "container shrink"
         }, holdTime)
     }, breatheTime)
 }
